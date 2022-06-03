b3c91161b37c7b3dc11c41d4cf306c7962d36f76
Mariana Leal <102970349+marisleais@users.noreply.github.com>
2022-06-02 21:44:26 -0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,7 +1,3 @@
-/* var = variavés que podem ser alteradas
-const = constantes que NÃO podem ser alteradas
-function = para ser executada precisa ser chamada
-*/
 
 var currentNumberw = document.getElementById('currentNumber')
 
@@ -29,19 +25,12 @@ function increment(){
     currentNumberw.innerHTML = currentNumber;
 }
 
-/*
-document.querySelector('#restartBtn').addEventListener('click', function(){
-    window.location.reload();
-    return false;
-  }); */
-
- 
 
   function zero(){
     var currentNumberw = document.getElementById('currentNumber')
     var currentNumber = 0  
     currentNumberw.innerHTML = currentNumber; 
-    /*return [ currentNumber= 0 ] */
+    
     
   }
   
